ca028498b81f4c69103ff973b9e0fe8f8078e9f6
LowerLegs <hsuk0409@gmail.com>
2021-03-06 14:24:42 +0900

    동적 메모리 할당해서 문자열 토큰화 한번에 처리하기 완료.. 하지만 이중 포인터 크기 넉넉히 잡아서 처리.. 이중 포인터 크기를 재할당 받는게 더 좋은 방법일 듯..

--- allocate-tokenize/main.c
+++ allocate-tokenize/main.c
@@ -7,7 +7,6 @@ char** tokenize_malloc(char* str, const char* delims);
 
 char** tokenize_malloc(char* str, const char* delims)
 {
-    char** tmp_pp;
     char** result_pp;
     char tmp_str[4096];
     char* tmp_p;
@@ -16,15 +15,17 @@ char** tokenize_malloc(char* str, const char* delims)
     printf("[func] str: %s\n", str);
     strncpy(tmp_str, str, strlen(str));
     
+    result_pp = malloc(sizeof(char*) * strlen(str));
     tmp_p = strtok(tmp_str, delims);
     while (tmp_p != NULL) {
-        tmp_pp = malloc(sizeof(char*));
-        tmp_pp[i] = malloc(strlen(tmp_p) + 1);
-        tmp_pp[i++] = tmp_p;
-        printf("[func] token pointer: %s\n", tmp_pp[i - 1]);
-        result_pp = realloc(tmp_pp, sizeof(char*) * i);
+        /* result_pp = malloc(sizeof(char*)); */
+        result_pp[i] = malloc(strlen(tmp_p) + 1);
+        strncpy(result_pp[i++], tmp_p, strlen(tmp_p) + 1);
+        printf("[func] token pointer: %s\n", result_pp[i - 1]);
+        /* result_pp = realloc(result_pp, sizeof(char*) * i); */
         tmp_p = strtok(NULL, delims);
     }
+    result_pp[i] = NULL;
     printf("[func end] last token: %s\n", *result_pp);
     
     return result_pp;
@@ -36,16 +37,30 @@ int main(void)
     char** tt;
     char str[] = "Hello, justin! haha!";
     
-    token = tokenize_malloc(str, ",!");
+    token = tokenize_malloc(str, " ");
     tt = token;
 
-    printf("token: %s\n", *tt);
-    assert(strcmp(*tt++, "Hello") == 0);
+    assert(strcmp(*tt++, "Hello,") == 0);
+    assert(strcmp(*tt++, "justin!") == 0);
+    assert(strcmp(*tt++, "haha!") == 0);
+
+    {
+        size_t i;
+        tt = token;
+        for (i = 0; *tt != NULL; ++i) {
+            printf("token%d: %s\n", i, *tt++);
+        }
+    }
     
     tt = token;
+
+    printf("pre free token: %s\n", *tt);
     while (*tt != NULL) {
-        free(tt++);
+        printf("free toekn: %s\n", *tt);
+        free(*tt++);
+        printf("post free toekn: %s\n", *tt);
     }
+    printf("finish free tt!!\n");
     free(token);
     
     return 0;

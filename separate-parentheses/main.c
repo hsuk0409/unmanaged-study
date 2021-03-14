554fe64b7727f3bb2c3f63fd81d26e4430accffc
LowerLegs <hsuk0409@gmail.com>
2021-03-14 23:52:48 +0900

    괄호 쌍끼리 분리해서 인데스 저장하기 함수 구현 중

--- separate-parentheses/main.c
+++ separate-parentheses/main.c
@@ -2,6 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    size_t s_index;
+    size_t e_index;
+} pts_t;
+
+size_t get_matching_pts(pts_t* ptses, const char* str);
+
+size_t get_matching_pts(pts_t* ptses, const char* str)
+{
+    pts_t tmp_pts;
+    size_t result = 0;
+    size_t pts_index = 0;
+    char* p = (char*)str;
+    size_t str_len = strlen(str);
+    int i;
+    int k;
+    
+    for (i = 0; i < (int)str_len) {
+        if (p[i] == ')') {
+            printf("end index: %d\n", i);
+            for (k = i - 1; k >= 0; --k) {
+                if (p[k] != '\0' && p[k] == '(') {
+                    printf("start index: %d\n", k);
+                    tmp_pts.s_index = (size_t)k;
+                    tmp_ots.e_index = (size_t)i;
+                    printf("temp pts start index: %d\n", tmp_pts.s_index);
+                    printf("temp pts end index: %d\n", tmp_pts.e_index);
+                    ptses[pts_index++] = tmp_pts;
+                    
+                    
+                }
+            } 
+        }
+    }
+    
+    return result;
+}
+
 int main(void)
 {
     

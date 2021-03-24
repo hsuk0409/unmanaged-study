a7a8b68e73afb230a0dd08b8f044cc69ffae91a9
LowerLegs <hsuk0409@gmail.com>
2021-03-24 22:11:37 +0900

    간단한 해시 함수 연습

--- hash-func/main.c
+++ hash-func/main.c
@@ -1,11 +1,13 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 size_t hash_int(int value)
 {
     return value;
 }
 
-size_t hash_float(flaot value)
+size_t hash_float(float value)
 {
     return *((size_t*)&value);
 }
@@ -19,8 +21,42 @@ size_t hash_data(const void* data, size_t length)
     p = data;
     hash = 0;
     for (i = 0; i < length; ++i) {
-        hash = 65599 * hash + *pp++;
+        hash = 65599 * hash + *p++;
     }
 
     return hash ^ (hash >> 16);
+}
+
+int main(void)
+{
+    {
+        int a = 12345;
+        printf("%d\n", hash_int(a));
+    }
+
+    {
+        float a = 12345.123f;
+        printf("%d\n", hash_float(a));
+    }
+
+    {
+        printf("%u\n", hash_data("Jusin!!", strlen("justin")));
+    }
+
+    {
+        typedef struct {
+            unsigned char age;
+            unsigned int id;
+            char name[64];
+        } employee_t;
+
+        employee_t person;
+        memset(&person, 0, sizeof(employee_t));
+        person.age = 29;
+        person.id = 18274192;
+        strcpy(person.name, "justin");
+        printf("%d\n", hash_data(&person, sizeof(employee_t)));
+    }
+
+    return 0;
 }

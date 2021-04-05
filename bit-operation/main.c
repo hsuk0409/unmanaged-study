520a7ce664e4b8283580ff927cf3a58edf9657ef
LowerLegs <hsuk0409@gmail.com>
2021-04-06 07:20:02 +0900

    '해커의 기쁨' 책에 있는 논리 연산 테스트: 가산기로 감산기 만드는 연산

---
+++ bit-operation/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    {
+        size_t x = 11;
+        size_t y = 10;
+        size_t result;
+
+        result = x + ~y + 1;
+
+        printf("%d\n", result);
+    }
+
+    return 0;
+}

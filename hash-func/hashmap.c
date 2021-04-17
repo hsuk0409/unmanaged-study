0e6790112a6871940098fd207afea25b753f4930
LowerLegs <hsuk0409@gmail.com>
2021-04-17 10:41:30 +0900

    해시키 반환하는 간단한 해시함수 추가

--- hash-func/hashmap.c
+++ hash-func/hashmap.c
@@ -4,4 +4,15 @@
 
 #include "hashmap.h"
 
+size_t hash_function(const char* key);
 
+size_t hash_function(const char* key)
+{
+    size_t code = 0;
+    const char* c = key;
+
+    while (*c != '\0') {
+        code += *c++;
+    }
+    return code;
+}

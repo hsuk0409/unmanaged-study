b37c31008251fba3d905f0c0eb5add95de6665f5
LowerLegs <hsuk0409@gmail.com>
2021-07-05 23:47:39 +0900

    선택 정렬 구현

--- sort/main.c
+++ sort/main.c
@@ -1,10 +1,32 @@
 #include<stdio.h>
 #include "select_sort.h"
 
+#define ARR_SIZE (7)
+
+void printArr(int* arr);
+
 int main(void)
 {
-    int nums[10] = { 6, 4, 10, 22, 1, 3, 15 };
-    select_sort(nums);
+    int nums[ARR_SIZE] = { 6, 4, 10, 22, 1, 3, 15 };
+    
+    printf("Before sort!\n");
+    printArr(nums);
+    
+    select_sort(nums, ARR_SIZE);
+    
+    printf("After sort!\n");
+    printArr(nums);
     
     return 0;
+}
+
+void printArr(int* arr)
+{
+    int* tmp = arr;
+    int i;
+    
+    for(i = 0; i < ARR_SIZE; ++i) {
+        printf("%d ", tmp[i]);
+    }
+    printf("\n");
 }
--- sort/select_sort.h
+++ sort/select_sort.h
@@ -3,11 +3,37 @@
 
 #include <stdio.h>
 
-void select_sort(int* nums);
+void select_sort(int* nums, size_t length);
 
-void select_sort(int* nums)
+size_t get_min_index(int* nums, size_t start, size_t length);
+
+void select_sort(int* nums, size_t length)
 {
-    printf("Number : %d\n", *nums);
+    int* tmp_p = nums;
+    size_t i;
+    size_t min;
+
+    for(i = 0; i < length - 1; ++i) {
+        int tmp;
+        min = get_min_index(tmp_p, i, length);
+        tmp = tmp_p[i];
+        tmp_p[i] = tmp_p[min];
+        tmp_p[min] = tmp;
+    }
+}
+
+size_t get_min_index(int* nums, size_t start, size_t length)
+{
+    size_t i;
+    size_t min = start;
+    int* tmp = nums;
+    for (i = start + 1; i < length; ++i) {
+        if(tmp[min] > tmp[i]) {
+            min = i;
+        }
+    }
+
+    return min;
 }
 
 #endif
